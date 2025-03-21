42e932340b31a2c06863bf1241759aca6fde2d8a
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2025-03-21 19:09:12 +0200

    update

--- Program.cs
+++ Program.cs
@@ -5,21 +5,47 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+
+{
+    app.UseXContentTypeOptions();
+    app.UseReferrerPolicy(options => options.NoReferrer());
+    app.UseXXssProtection(options => options.EnabledWithBlockMode());
+    app.UseXfo(opt => opt.Deny());
+    app.UseCspReportOnly(options =>
+        options
+            .BlockAllMixedContent()
+            .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+            .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+            .FormActions(s => s.Self())
+            .FrameAncestors(s => s.Self())
+            .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+            .ScriptSources(s => s.Self())
+    );
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+else
+{
+    app.Use(
+        async (context, next) =>
+        {
+            context.Response.Headers.Append("Strict-Transport-Security", "max-age=31536000");
+            await next();
+        }
+    );
+}
+
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
